88762bf4d0c226db6e0835bcb1891ff1e28b165e
Mahyuddin Susanto <udienz@gmail.com>
2020-08-12 22:44:00 +0700

    Remove PS1
     - Remove PS1
     - Add bybu config and install
     - Change default gpg key

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 set -x
-sudo apt install gnupg2 -y
+sudo apt install byobu gnupg2 -y
 
 #if grep -Fxq "dot-bash" ~/.bashrc
 if grep -E ".dot-bash$" ~/.bashrc
@@ -14,6 +14,9 @@ else
     echo -e "\n. \$HOME/.dot-bash" >> ~/.bashrc
 fi
 
+update-alternatives --set editor /usr/bin/vim.basic
 source ~/.bashrc
 cp dot-vim ~/.vimrc
 cp gitconfig ~/.gitconfig
+cp byobu-status ~/.byobu/status
+cp byobu-datetime.tmux ~/.byobu/datetime.tmux
